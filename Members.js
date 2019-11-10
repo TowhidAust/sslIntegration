835aecee708e11cc185450e747ce022f62b73bfa
TowhidAust <towhidaustcse33@gmail.com>
2019-11-11 01:26:55 +0600

    init

---
+++ Members.js
@@ -0,0 +1,18 @@
+const members = [{
+        id: 1,
+        name: 'john Doe',
+        email: 'johndoe@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Towhidul Islam',
+        email: 'towhid@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Abraham Linkon',
+        email: 'abraham@gmail.com'
+    }
+];
+
+module.exports = members;
