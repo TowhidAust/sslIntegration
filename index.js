aea945b1d998db1485aff43fb8d8861a004f687f
TowhidAust <007towhid2016@gmail.com>
2019-11-11 11:47:58 +0600

    express crash course done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
@@ -10,9 +12,24 @@ const app = express();
 // init middleware
 // app.use(logger);
 
+// handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); 
+
+// homepage route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member app',
+    members : members
+}));
 
 // set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// members api route
 app.use('/api/members', require('./routes/api/members'));
 
 
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const uuid = require('uuid');
 const router = express.Router();
 const members = require('../../Members');
 
@@ -12,6 +13,65 @@ router.get('/:id', (req, res) => {
 
     if (found) {
         res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({
+            msg: `no member with the id of ${req.params.id}`
+        });
+    }
+});
+
+// create a member
+router.post('/', (req, res) => {
+    // res.send(req.body);
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: 'active'
+    }
+
+    // check 
+    if (!newMember.name || !newMember.email) {
+       return res.status(400).json({ msg: 'please include a name and email' });
+    }
+
+    members.push(newMember);
+    res.json(members);
+    // res.redirect('/');
+});
+
+
+// update member
+router.put('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        const updateMember = req.body;
+        members.forEach(member => {
+            if (member.id === parseInt(req.params.id)) {
+                member.name = updateMember.name ? updateMember.name : member.name;
+                member.email = updateMember.email ? updateMember.email : member.email;
+                res.json({ msg: 'member updated', member });
+            }
+        })
+    } else {
+        res.status(400).json({
+            msg: `no member with the id of ${req.params.id}`
+        });
+    }
+});
+
+
+// delete member
+// get single member
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        res.json({
+            msg: 'members Deleted',
+            members: members.filter(member => member.id !== parseInt(req.params.id))
+        });
     } else {
         res.status(400).json({
             msg: `no member with the id of ${req.params.id}`
